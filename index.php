1599220e521188471320270c2a303d631f8ec9e7
Edward97-Zalomay <zalomay.edikk46@gmail.com>
2018-05-01 22:28:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -2,60 +2,70 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-        <title>Megapolis</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<title>Megapolis</title>
 		<link rel="stylesheet" href="./css/jquery.fullpage.min.css" type="text/css">
-       	<link rel="stylesheet" href="./css/style.css" type="text/css">
-        <script type="text/javascript" src="./js/jquery-2.1.1.min.js"></script>
-        <script type="text/javascript" src="./js/jquery.fullpage.min.js"></script> 
-        <script type="text/javascript" src="./js/app.js"></script>      
+		<link rel="stylesheet" href="./css/style.css" type="text/css">
+		<script type="text/javascript" src="./js/jquery-2.1.1.min.js"></script>
+		<script type="text/javascript" src="./js/jquery.fullpage.min.js"></script> 
+		<script type="text/javascript" src="./js/app.js"></script>      
 	</head>
 	<body class="common-home">
-        <div id="fullpage">
-            <section  class="section" id="section0">
-                test
-            </section>
-            
-            <section  class="section" id="section1">
-                test2
-            </section>
-            
-            <section  class="section" id="section2">
-                test3
-            </section>
-            
-            <section  class="section" id="section3">
-                test4
-            </section>
-        </div>
-        <div id="fp-nav" class="left">
-            <ul>
-                <li>
-                    <a data-menuanchor="test1" href="#" class="">
-                        <span>1</span>
-                    </a>
-                    <div class="fp-tooltip left">fullPage.js</div>
-                </li>
-                <li>
-                    <a data-menuanchor="test2" href="#" class="">
-                        <span>2</span>
-                    </a>
-                    <div class="fp-tooltip left">Powerful</div>
-                </li>
-                <li>
-                    <a data-menuanchor="test3" href="#" class="">
-                        <span>3</span>
-                    </a>
-                    <div class="fp-tooltip left">Amazing</div>
-                </li>
-                <li>
-                    <a data-menuanchor="test4" href="#" class="">
-                        <span>4</span>
-                    </a>
-                    <div class="fp-tooltip left">Simple</div>
-                </li>
-            </ul>
-        </div>
+		<div id="fullpage">
+		    <section  class="section" id="section0">
+			test
+		    </section>
+
+		    <section  class="section" id="section1">
+			test2
+		    </section>
+
+		    <section  class="section" id="section2">
+			test3
+		    </section>
+
+		    <section  class="section" id="section3">
+			test4
+		    </section>
+			
+		    <section  class="section" id="section4">
+			test5
+		    </section>
+		</div>
+		<div id="fp-nav" class="left">
+		    <ul>
+			<li>
+			    <a data-menuanchor="test1" href="#" class="">
+				<span>1</span>
+			    </a>
+			    <div class="fp-tooltip left">fullPage.js</div>
+			</li>
+			<li>
+			    <a data-menuanchor="test2" href="#" class="">
+				<span>2</span>
+			    </a>
+			    <div class="fp-tooltip left">Powerful</div>
+			</li>
+			<li>
+			    <a data-menuanchor="test3" href="#" class="">
+				<span>3</span>
+			    </a>
+			    <div class="fp-tooltip left">Amazing</div>
+			</li>
+			<li>
+			    <a data-menuanchor="test4" href="#" class="">
+				<span>4</span>
+			    </a>
+			    <div class="fp-tooltip left">Simple</div>
+			</li>
+			 <li>
+			    <a data-menuanchor="test5" href="#" class="">
+				<span>5</span>
+			    </a>
+			    <div class="fp-tooltip left">Simple</div>
+			</li>
+		    </ul>
+		</div>
 	</body>
 </html>
